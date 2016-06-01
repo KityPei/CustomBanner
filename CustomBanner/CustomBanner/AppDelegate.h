84d8c045ac8b64f707c21fff9a00ae4d4cb0cb53
Kity_Pei <Kity_Pei@KityPeideMacBook-Pro.local>
2016-06-02 00:03:32 +0800

    添加空项目

---
+++ CustomBanner/CustomBanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomBanner
+//
+//  Created by Kity_Pei on 16/6/2.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomBanner/CustomBanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomBanner
+//
+//  Created by Kity_Pei on 16/6/2.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
